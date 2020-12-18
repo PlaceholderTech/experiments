191c45dafb9378793d6cefe72cc0a73adcead445
Manu Arora <mannuarora700@gmail.com>
2020-12-18 17:09:45 +0530

    add Monaco, playground component & editor comp

--- components/MonacoEditor.js
+++ components/MonacoEditor.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { ControlledEditor } from '@monaco-editor/react';
 
-export default function MonacoEditor() {
-  return <div></div>;
+export default function MonacoEditor(props) {
+  const { language, displayName, value, onChange, height, width } = props;
+  function handleChange(ev, value) {
+    onChange(value);
+  }
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        <button>{displayName}</button>
+      </div>
+      <ControlledEditor
+        // onBeforeChange={handleChange}
+        value={value}
+        className=""
+        height={height}
+        width={width}
+        language={language}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import Navigation from '@components/Navigation';
+import Playground from '@components/Playground';
 
 export default function Home() {
   return (
@@ -16,9 +17,33 @@ export default function Home() {
       </div>
 
       <main>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div className="">
+          <header className="flex flex-col justify-center items-center mb-52">
+            <h1 className="font-bold text-9xl mt-52 mb-4 text-blue-300">
+              PLAYGROUND
+            </h1>
+            <p className="text-gray-500 text-lg">
+              Components, Playground, Themes, and Layouts.
+            </p>
+            {/* <svg
+              width="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="#273036"
+                fill-opacity="1"
+                d="M0,224L48,192C96,160,192,96,288,101.3C384,107,480,181,576,213.3C672,245,768,235,864,213.3C960,192,1056,160,1152,154.7C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+              ></path>
+            </svg> */}
+          </header>
+
+          <section className="">
+            <div class="editor m-6">
+              <Playground className="m-6" />
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
